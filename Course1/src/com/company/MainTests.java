aff127b749976ce1cdb4d518eb3b8158012518a6
RodrigueMedor <rodriguemedor@yahoo.fr>
2020-05-30 10:49:57 -0400

    first test

---
+++ Course1/src/com/company/MainTests.java
@@ -0,0 +1,12 @@
+package com.company;
+
+ public class MainTests {
+
+    public static void main(String... args) throws Exception{
+	// write your code here
+        main("");
+        main();
+    }
+
+
+}
