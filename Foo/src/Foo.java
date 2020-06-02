4fe2723889f95a61c2c4ba6dafb44faf47ebaaa8
rmedor <rmedor@nexient.com>
2020-06-02 18:09:48 -0400

    Primitive data type with initialization, different of loops

---
+++ Foo/src/Foo.java
@@ -0,0 +1,47 @@
+
+public class Foo {
+	
+	public static void main(String[] args) {
+		
+//		boolean myBoolean;
+//		byte myByte;
+//		char myFirstChar, mySecondChar;
+//		short myShort;
+//		long mylong;
+//		float myLong;
+//		double myFirstDouble, mySecondDouble;
+		
+		byte b = 127;
+		char c = 127;
+		short s = 127;
+		int i = 127;
+		
+		long l1 = 127l;
+		long l2 = 127L;
+		
+		float  f1 = 127e02f;
+		float  f2 = 127e02F;
+		
+		double d1 = 127d;
+		double d2 = 127D;
+		
+		boolean isTrue = false;
+		
+		System.out.println("b = "+ b);
+		System.err.println("c = :" +c);
+		System.out.println("s = :"+ s);
+		System.out.println("i = : "+i);
+		
+		System.out.println("l1 = :"+l1);
+		System.out.println("l2 = :"+l2);
+		
+		System.out.println("f1 = :"+f1);
+		System.out.println("f2 = :"+f2);
+		
+		System.out.println("d1 = "+d1);
+		System.out.println("d2 = "+d2);
+		System.out.println("isTrue :"+isTrue);
+		
+	}
+
+}
