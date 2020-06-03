58ded4fbb04ab70f41e05fb4b4b0aae270df89c1
rmedor <rmedor@nexient.com>
2020-06-03 00:35:48 -0400

    test final

--- Foo/src/TestPerson.java
+++ Foo/src/TestPerson.java
@@ -5,21 +5,37 @@ Topic: Identify the Scope of a variable
 */
 
 public class TestPerson {
-    public static void main(String[] args) {
 
-        // First we'll create a person, JOE using 2 args constructor.
-        Person joe = new Person("JOE", "55");
+	public static void main(String[] args) {
+		// First we'll create a person, JOE using 2 args constructor.
+		Person joe = new Person("JOE", "55");
+		// Next we'll create a person, using 1 arguments
+		Person deb = new Person("DEB", "85");
+		// And now the noargs constructor, and using setters
+		Person ann = new Person();
+		ann.setAge("21");
+		ann.setInstanceName("ANN");
+		System.out.println(joe);
+		System.out.println(deb);
+		System.out.println(ann);
+	}
 
-        // Next we'll create a person, using 1 arguments
-        Person deb = new Person("DEB", "85");
+	public String getDecade(int age) {
+		String decadeString = "";
 
-        // And now the noargs constructor, and using setters
-        Person ann = new Person();
-        ann.setAge("21");
-        ann.setInstanceName("ANN");
-
-        System.out.println(joe);
-        System.out.println(deb);
-        System.out.println(ann);
-    }
+		// This variable holds the maximum decade
+		int decadeNumber = age / 10;
+		// add variable j
+		int j = 0;
+		for (int i = j = 0; i < (decadeNumber + 1); i++, j++) {
+			// local loop block variable named decade, scope is loop
+			String decade = "Decade " + (age / 10);
+			if (i == (decadeNumber)) {
+				decadeString = decade;
+			}
+		}
+		int modyear = age % 10;
+		decadeString += ", Year" + modyear;
+		return decadeString;
+	}
 }
